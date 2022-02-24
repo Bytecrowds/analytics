74ffdfcef876a84c5f67eefd772be31782fd48e6
TudorZgimbau <tudor.zgimbau@gmail.com>
2022-02-24 16:05:59 +0200

    improved analytics code and added the per day option

--- analytics/analytics.go
+++ analytics/analytics.go
@@ -9,20 +9,60 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"context"
+
+	"strings"
+	"time"
 )
 
 var IPanalytics = database.IPanalytics
+var DayAnaytics = database.DayAnalytics
 
 func InterceptRequest(ginContext *gin.Context) {
-	var data, result models.Request
+	var data models.Request
 	ginContext.BindJSON(&data)
 
-	filter := bson.D{{"ip", data.IP}}
-	IPstat := bson.D{{"ip", data.IP}, {"hits", 0}}
+	filterIPstat := bson.D{{"ip", data.IP}}
+	IPstat := bson.D{{"ip", data.IP}, {"hits", 1}}
 
-	IPanalytics.FindOne(context.TODO(), filter).Decode(&result)
+	filterDayStat := bson.D{
+		{"day", strings.TrimSpace(time.Now().String()[:11])},
+	}
 
-	if result.IP == "" {
+	var searchIP models.IPstat
+	IPanalytics.FindOne(context.TODO(), filterIPstat).Decode(&searchIP)
+
+	var searchDay models.DayStat
+	DayAnaytics.FindOne(context.TODO(), filterDayStat).Decode(&searchDay)
+
+	if searchIP.IP == "" {
 		IPanalytics.InsertOne(context.TODO(), IPstat)
+	} else {
+		modifiedIPstat := bson.D{{"$set", bson.D{{"hits", searchIP.Hits + 1}}}}
+		IPanalytics.UpdateOne(context.TODO(), filterIPstat, modifiedIPstat)
+	}
+
+	if searchDay.Day == "" {
+		DayStat := bson.D{
+			{"day", strings.TrimSpace(time.Now().String()[:11])},
+			{"hits", 1},
+			{"addresses", []string{data.IP}},
+		}
+		DayAnaytics.InsertOne(context.TODO(), DayStat)
+	} else {
+		var found bool = false
+		for _, address := range searchDay.Addresses {
+			if data.IP == address {
+				found = true
+				break
+			}
+		}
+		// we will increment the day hits only if the request comes from a new IP
+		if found == false {
+			modifiedDayStat := bson.D{{"$set", bson.D{
+				{"hits", searchDay.Hits + 1},
+				{"addresses", append(searchDay.Addresses, data.IP)},
+			}}}
+			DayAnaytics.UpdateOne(context.TODO(), filterDayStat, modifiedDayStat)
+		}
 	}
 }
