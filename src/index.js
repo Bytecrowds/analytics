420142055ca2a2d15bc1b9dd1cd121dba47e374a
TudorZgimbau <tudor.zgimbau@gmail.com>
2022-07-15 16:53:56 +0300

    Refactored to send less requests to DB

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const router = new Router();
 
 // Because cloudflare workers expose the env on fetch, we need to manually set the vars
 const redis = new Redis({
-  url: "<UPSTASH_REDIS_REST_URL>",
-  token: "<UPSTASH_REDIS_REST_TOKEN>",
+  url: "https://eu2-large-spider-30471.upstash.io",
+  token:
+    "AXcHASQgZjU1M2Q2MjYtNzdhNy00NjY1LWIwZTktMDY1ZjM0MzFmNGQ0ZjNhMGQ4ZWI0MDA3NGY1OTg4YjU4NjQzN2E2ZDA3NzY=",
 });
 
 router.get("/bytecrowd/:bytecrowd", async ({ req, res }) => {
@@ -21,21 +22,20 @@ router.post("/update", async ({ req, res }) => {
   name = req.body.name;
   text = req.body.text;
   language = req.body.language;
-  if (!(await redis.exists(name)))
+
+  const storedBytecrowd = await redis.hgetall(name);
+  if (!storedBytecrowd)
     // if the bytecrowd doesn't exist, create it
-    await redis.hmset(name, { text: text, language: "javascript()" });
-  else {
-    const storedBytecrowd = await redis.hgetall(name);
+    await redis.hmset(name, { text: text, language: "javascript" });
+  else if (
     // if at least one element(text/language) changed , update the bytecrowd
-    if (
-      storedBytecrowd.text !== text ||
-      storedBytecrowd.language !== language
-    ) {
-      // if the request doesn't contain a new text/language, use the current one
-      if (!text) text = storedBytecrowd.text;
-      if (!language) language = storedBytecrowd.language;
-      await redis.hmset(name, { text: text, language: language });
-    }
+    storedBytecrowd.text !== text ||
+    storedBytecrowd.language !== language
+  ) {
+    // if the request doesn't contain a new text/language, use the current one
+    if (!text) text = storedBytecrowd.text;
+    if (!language) language = storedBytecrowd.language;
+    await redis.hmset(name, { text: text, language: language });
   }
 });
 
